d87bf4565018d4775619c388c133fd90529506d4
james2doyle <james2doyle@gmail.com>
2015-09-02 13:34:29 -0700

    fix examples for checkboxes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
   hljs.initHighlightingOnLoad();
-  var items = ['number', 'select', 'area', 'range', 'radio', 'time', 'date', 'color', 'datetime', 'datetime-local', 'week', 'file', 'hidden', 'image'];
+  var items = ['number', 'select', 'area', 'range', 'radio', 'checkbox', 'time', 'date', 'color', 'datetime', 'datetime-local', 'week', 'file', 'hidden', 'image'];
   var sb = [];
   for (var i = 0; i < items.length; i++) {
     sb[i] = SimpleBinder(items[i], function(e, t) {
       console.log(e.value);
     });
   }
-  var checkbox = SimpleBinder('checkbox', {
+  var checkbox = SimpleBinder('checked', {
     watch: 'checked' // what controller attribute are we watching?
   });
 });
